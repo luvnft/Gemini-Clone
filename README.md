21bae62db247b6bcae34f52e865c1558e608d27d
Ayane Takagi <a08028701123@gmail.com>
2024-12-16 04:45:23 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Gemini-Clone
-A React JS app that uses the Gemini API to create an AI-powered conversational interface. This project demonstrates how to integrate generative AI models into web applications.
+
+This project is a React app that uses the Gemini API to create an AI-powered conversational interface.
+
+## React + Vite Setup
+
+This template provides a minimal setup to get React working in Vite with Hot Module Replacement (HMR) and some ESLint rules.
+
+Currently, two official plugins are available:
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README)
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for faster compilation.
---
+++ src/config/gemini.js
@@ -0,0 +1,42 @@
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  console.log("Environment Variables:", import.meta.env);
+  console.log("API Key:", import.meta.env.VITE_GEMINI_API_KEY);
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+
+  if (!apiKey) {
+    throw new Error("VITE_GEMINI_API_KEY is not defined. Please set it in your .env file.");
+  }
+
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash-exp",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+  export default run;
