e0ce519f52720774a6c8168ea9b9828374c289e4
Ayane Takagi <a08028701123@gmail.com>
2024-12-18 00:36:34 +0100

    Fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 相対パスでビルドする設定
+  build: {
+    outDir: 'dist' // 出力ディレクトリを dist に指定
+  }
 })
